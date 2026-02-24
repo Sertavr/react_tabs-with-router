b4a718b3c020dd1e000331e23421246b905edd9b
Sertavr <pampuh.net@gmail.com>
2026-02-24 18:23:03 +0200

    added task solution

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ const getClassName = ({ isActive }: { isActive: boolean }) => {
 
 export const App = () => (
   <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
     <nav
       className="navbar is-light is-fixed-top is-mobile has-shadow"
       data-cy="Nav"
@@ -27,20 +26,6 @@ export const App = () => (
 
     <div className="section">
       <div className="container">
-        {/* <div className="tabs is-boxed">
-          <ul>
-            <li data-cy="Tab" className="is-active">
-              <a href="#/">Tab 1</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 2</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 3</a>
-            </li>
-          </ul>
-        </div> */}
-
         <Outlet />
       </div>
     </div>
